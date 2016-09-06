d0e53384f710a2d8feb5e7cfabb481d0047a8e98
Josh <joshuaj004@gmail.com>
2016-09-05 21:01:30 -0700

    Created the percentage_maker to set up the preliminary key finding algorithm.

--- vigenere.py
+++ vigenere.py
@@ -1,4 +1,5 @@
 __author__ = 'Josh'
+import string
 
 
 def main():
@@ -8,9 +9,36 @@ def main():
     grid_printer(matrix)
     coincidence_list = coincidence_finder(encrypted, matrix)
     print(coincidence_list)
+    print(percentage_maker(encrypted, 2))
+
+
+def percentage_maker(encrypted, keyLength):
+    """
+    Takes in the encrypted message and the key length and creates a frequency percentage
+    :param encrypted: The encrypted message.
+    :param keyLength: The length of the key.
+    :return: A list of percents for how often a letter occurs
+    """
+    freq_dict = {letter: 0 for letter in string.ascii_uppercase}
+
+    for x in range(len(encrypted)):
+        if x % keyLength == 0:
+            freq_dict[encrypted[x]] += 1
+
+    percent_list = []
+    for letter in string.ascii_uppercase:
+        percent_list.append(freq_dict[letter] / len(encrypted))
+
+    return percent_list
 
 
 def coincidence_finder(encrypted, matrix):
+    """
+    Calculates the number of coincidences per possible string
+    :param encrypted: The encrypted message
+    :param matrix: The grid of shifted messages
+    :return: The list of coincidences in order, used to determine the key length.
+    """
     coincidence_list = []
     for offset in matrix:
         tempCount = 0
